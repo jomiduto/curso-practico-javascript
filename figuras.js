aedc00acf9a10d879f2011c4fcec4fc9d0e720e9
Jose Dueñas <jomiguel097@gmail.com>
2021-10-26 23:14:18 -0500

    Integración de javascript con HTML, integrando funciones con onclick

--- figuras.js
+++ figuras.js
@@ -43,3 +43,72 @@ function areaCirculo(radio) {
 }
 
 console.groupEnd();
+
+// Aquí interactuamos con el HTML
+
+function calcularPerimetroCuadrado() {
+    const input = document.getElementById("inputCuadrado");
+    const value = input.value;
+
+    const perimetro = perimetroCuadrado(value);
+    alert(perimetro);
+}
+
+function calcularAreaCuadrado() {
+    const input = document.getElementById("inputCuadrado");
+    const value = input.value;
+
+    const area = areaCuadrado(value);
+    alert(area);
+}
+
+function calcularPerimetroTriangulo() {
+    const lado1 = document.getElementById("lado1");
+    const valueLado1 = parseInt(lado1.value);
+
+    const lado2 = document.getElementById("lado2");
+    const valueLado2 = parseInt(lado2.value);
+
+    const lado3 = document.getElementById("lado3");
+    const valueLado3 = parseInt(lado3.value);
+
+    const perimetro = perimtetroTriangulo(valueLado1, valueLado2, valueLado3);
+    alert(perimetro);
+}
+
+function calcularAreaTriangulo() {
+    const base = document.getElementById("lado3");
+    const valueBase = base.value;
+
+    const altura = document.getElementById("alturaTriangulo");
+    const valueAltura = altura.value;
+
+    const area = areaTriangulo(valueBase, valueAltura);
+    alert(area);
+}
+
+function calcularDiametroCirculo() {
+    const radio = document.getElementById("radio");
+    const valueRadio = radio.value;
+
+    const diametro = diametroCirculo(valueRadio);
+    alert(diametro)
+}
+
+function calcularAreacirculo() {
+    const radio = document.getElementById("radio");
+    const valueRadio = radio.value;
+
+    const area = areaCirculo(valueRadio);
+    alert(area)
+}
+
+function calcularPerimetroCirculo() {
+    const radio = document.getElementById("radio");
+    const valueRadio = radio.value;
+
+    const diametro = diametroCirculo(valueRadio);
+
+    const perimetro = perimetroCirculo(valueRadio);
+    alert(perimetro);
+}
