45a7d351c9534c4f4ad16dff4be35c2b60fca8cd
Jose Dueñas <jomiguel097@gmail.com>
2021-12-01 11:28:44 -0500

    Modificación del código para que el descuento se haga por medio de cupones

--- descuentos.js
+++ descuentos.js
@@ -1,6 +1,21 @@
 // const precioOriginal = 120;
 // const descuento = 18;
 
+const coupons = [
+    {
+        name: 'cupon_platzi',
+        discount: 15,
+    },
+    {
+        name: 'cupon_bienvenida',
+        discount: 30,
+    },
+    {
+        name: 'cupon_miguelito',
+        discount: 25,
+    },
+];
+
 function calcularPrecioconDescuento(precio, descuento) {
     const porcentajePrecioConDescuento = 100 - descuento;
     const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
@@ -12,11 +27,23 @@ function buttonPriceDiscount() {
     const inputPrice = document.getElementById('inputPrice');
     const priceValue = inputPrice.value;
 
-    const inputDiscount = document.getElementById('inputDiscount');
-    const discountValue = inputDiscount.value;
+    const inputCoupon = document.getElementById('inputCoupon');
+    const couponValue = inputCoupon.value;
 
-    const precioconDescuento = calcularPrecioconDescuento(priceValue, discountValue);
+    const isCouponValueValid = function(coupon) {
+        return coupon.name === couponValue;
+    };
 
-    const resultP = document.getElementById('resultPrice');
-    resultP.innerText = 'El precio con descuento son: $ ' + precioconDescuento;
+    const userCoupon = coupons.find(isCouponValueValid);
+
+    if(!userCoupon) {
+        alert("El cupón " + userCouponValue + " no es válido");
+    }else {
+        const descuento = userCoupon.discount;
+        const precioconDescuento = calcularPrecioconDescuento(priceValue, descuento);
+
+        const resultP = document.getElementById('resultPrice');
+        resultP.innerText = 'El precio con descuento son: $ ' + precioconDescuento;
+    }
+
 }
